0f18d43c088d866760f22a1f583fa4f452ad4b03
Kshitij Sharma <sharmakshitij250@gmail.com>
2023-10-23 19:50:57 +0530

    Ad cors

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
   
 const app = express(); 
 const PORT = 3000; 
+const cors = require("cors")
+
+app.use(cors())
   
 app.get('/', (req, res)=>{ 
     res.status(200); 
